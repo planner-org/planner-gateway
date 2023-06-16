066ec0ed29a8c654754cb77a295969bf4285afbb
IlyaVostrukhin <i.s.vostrukhin@yandex.ru>
2023-06-16 17:49:22 +0700

    Delete webflux dependency, comment Test

--- src/test/java/com/projects/planner/gateway/PlannerGatewayApplicationTests.java
+++ src/test/java/com/projects/planner/gateway/PlannerGatewayApplicationTests.java
@@ -1,13 +1,13 @@
-package com.projects.planner.gateway;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class PlannerGatewayApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.projects.planner.gateway;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class PlannerGatewayApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
